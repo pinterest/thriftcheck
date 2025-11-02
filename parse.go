eca3bc3471fd8bf6b3c2463984116f96a54ba850
Jon Parise <jon@indelible.org>
2025-11-01 21:20:37 -0400

    Introduce a caching FileParser (#126)
    This migrates the file parsing functions to a stateful FileParser type.
    This lets us implement a file-level cache so that repeated attempts to
    parse the same file (via includes) return the cached result.

--- parse.go
+++ parse.go
@@ -19,6 +19,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"go.uber.org/thriftrw/ast"
 	"go.uber.org/thriftrw/idl"
@@ -36,23 +37,70 @@ func Parse(r io.Reader) (*ast.Program, *idl.Info, error) {
 	return prog, cfg.Info, err
 }
 
-// ParseFile parses a Thrift file. The filename must appear in one of the
-// given directories.
-func ParseFile(filename string, dirs []string) (*ast.Program, *idl.Info, error) {
+type parsedFile struct {
+	prog *ast.Program
+	info *idl.Info
+}
+
+// FileParser caches parsed Thrift files to avoid re-parsing included files.
+// It is safe for concurrent use by multiple goroutines.
+type FileParser struct {
+	mu    sync.RWMutex
+	cache map[string]parsedFile
+	dirs  []string
+}
+
+// NewParser returns a new Parser with a list of directories that will be
+// searched for files.
+func NewFileParser(dirs []string) *FileParser {
+	return &FileParser{
+		cache: make(map[string]parsedFile),
+		dirs:  dirs,
+	}
+}
+
+// Parse parses Thrift document content with the given filename.
+func (p *FileParser) Parse(r io.Reader, filename string) (*ast.Program, *idl.Info, error) {
+	filename, err := filepath.Abs(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	p.mu.RLock()
+	cached, ok := p.cache[filename]
+	p.mu.RUnlock()
+
+	if ok {
+		return cached.prog, cached.info, nil
+	}
+
+	prog, info, err := Parse(r)
+	if err == nil {
+		p.mu.Lock()
+		p.cache[filename] = parsedFile{prog: prog, info: info}
+		p.mu.Unlock()
+	}
+
+	return prog, info, err
+}
+
+// ParseFile parses a Thrift file from its filename.
+func (p *FileParser) ParseFile(filename string) (*ast.Program, *idl.Info, error) {
 	if filepath.IsAbs(filename) {
 		if f, err := os.Open(filename); err == nil {
 			defer f.Close()
-			return Parse(f)
+			return p.Parse(f, filename)
 		}
 		return nil, nil, fmt.Errorf("%s not found", filename)
 	}
 
+	dirs := append([]string{filepath.Dir(filename)}, p.dirs...)
 	for _, dir := range dirs {
 		if f, err := os.Open(filepath.Join(dir, filename)); err == nil {
 			defer f.Close()
-			return Parse(f)
+			return p.Parse(f, f.Name())
 		}
 	}
 
-	return nil, nil, fmt.Errorf("%s not found in %s", filename, dirs)
+	return nil, nil, fmt.Errorf("%s not found in %s", filename, p.dirs)
 }
